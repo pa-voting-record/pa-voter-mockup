03c277a2a301ae655a69000579813ae9be5765a0
Aaron <aaron.long.c@gmail.com>
2016-12-17 20:45:42 -0500

    Created dynamic content function for single page app

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-"use strict";
+'use strict';
 
 (function() {
 
-  
+  //Profile Event Handler
+  let login = document.getElementById('login')
+  login.addEventListener('click', () => window.alert('Facebook OAuth Here'))
+
+  //Article
+  let article = iter => {
+  	console.log("hey")
+  	const container = document.getElementById('body_text')
+  	const text = {
+  		'start' : {
+  			title : 'Start Screen',
+  			text : 'Text Filler, Text Filler, Text Filler'
+  		}
+  	}
+  	let title = text => { return ('<span class="card-title">' + text + '</span>') }
+  	container.innerHTML = title(text[iter].title) + '<p>' + text[iter].text + '</p>'
+  }
+
+  article('start')
 
 })()
