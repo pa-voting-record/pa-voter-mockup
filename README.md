a1858e857e34ee35021a8e7c37fba27fa9d984ba
Aaron <aaron.long.c@gmail.com>
2016-12-19 21:07:56 -0500

    Made content dynamic based on key-bindings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #P.A. Voting Record App Mockup
 ###A Code for Philly Project
 
+Press enter or space to move the presentation forward
+Press to move backwards
--- js/index.js
+++ js/index.js
@@ -1,25 +1,61 @@
 'use strict';
 
 (function() {
+  //namespace for all app data
+  document.app = {}
 
   //Profile Event Handler
   let login = document.getElementById('login')
   login.addEventListener('click', () => window.alert('Facebook OAuth Here'))
 
   //Article
+  document.app.article_state = 0 //Should always be an int
   let article = iter => {
-  	console.log("hey")
   	const container = document.getElementById('body_text')
-  	const text = {
-  		'start' : {
-  			title : 'Start Screen',
-  			text : 'Text Filler, Text Filler, Text Filler'
-  		}
-  	}
+  	const text = [
+      {
+    			title : 'Start Screen',
+    			text : 'Text Filler, Text Filler, Text Filler'
+    	},
+      {
+        title : 'Logged In',
+        text : 'Finding your voting Record'
+      },
+      {
+        title : 'Calculating Your Voting Record',
+        text : 'Please wait while we get your badge'
+      }
+    ]
+    if (iter >= text.length) {
+      let cur = text.length - 1
+      iter = cur
+      document.app.article_state = cur
+    }
+    if (iter < 0) {
+      iter = 0
+      document.app.article_state = 0
+    }
   	let title = text => { return ('<span class="card-title">' + text + '</span>') }
   	container.innerHTML = title(text[iter].title) + '<p>' + text[iter].text + '</p>'
   }
 
-  article('start')
+  //Article Event listener
+   document.body.addEventListener("keypress", e => {
+      console.log("It's Firing")
+      console.log(document.app.article_state)
+      switch(e.charCode) {
+        case 13:
+          document.app.article_state++
+          break
+        case 32:
+          document.app.article_state++
+          break
+        case 98:
+          document.app.article_state--
+          break
+      }
+      article(document.app.article_state)
+    })
+   article(document.app.article_state)
 
 })()
